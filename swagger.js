381784ca41788ba084442f0c6847e4fce88ba109
aribyui <lizandro.arizpe@gmail.com>
2025-03-27 22:40:38 -0600

    swagger host updated

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'cse341-project2',
     description: 'API for managing books with CRUD operations using Node.js, Express, and MongoDB.'
   },
-  host: 'https://cse341-project2-abrs.onrender.com',
+  host: 'cse341-project2-abrs.onrender.com',
   schemes: ['https']
 };
 
